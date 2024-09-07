6bf29ef1294f0485a0b44bf29a18a1983ece02b0
Prashant L Mitba <156437998+mitbapl@users.noreply.github.com>
2024-09-08 01:00:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 import math
 
 app = Flask(__name__)
-os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-17-openjdk-amd64'
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64'
 os.environ['PATH'] = os.environ['JAVA_HOME'] + '/bin:' + os.environ['PATH']
 app.config['DEBUG'] = True
 app.config['UPLOAD_FOLDER'] = 'uploads/'  # Folder to store uploaded files
