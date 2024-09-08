7cbfd73800af9e1747c3d6fdf31a2fd177323a02
Prashant L Mitba <156437998+mitbapl@users.noreply.github.com>
2024-09-09 02:33:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -315,7 +315,10 @@ def upload_file():
             df_statement, df_turnover = process_hdfc(file_path)
             if df_statement is not None:
                 output_file = os.path.join(app.config['UPLOAD_FOLDER'], 'processed_hdfc.xlsx')
-                
+
+                # Adding your name to the last row as an author
+                author_row = pd.DataFrame({"Author": ["Prashant L. Mitba"]})  # Create a DataFrame for the author
+                df = pd.concat([df, author_row], ignore_index=True)  # Append the author DataFrame
                 # Save both DataFrames to the same Excel file, each in a separate sheet
                 with pd.ExcelWriter(output_file) as writer:
                     df_statement.to_excel(writer, sheet_name='Statement Data', index=False)
