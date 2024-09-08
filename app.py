53c07dd4a670dc5bf8a9c96a0d063555e6a5b6d6
Prashant L Mitba <156437998+mitbapl@users.noreply.github.com>
2024-09-09 01:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -329,4 +329,4 @@ def upload_file():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
