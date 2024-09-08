ccbaf49a77e65d275807b5f05972ccb857bc6c09
Prashant L Mitba <156437998+mitbapl@users.noreply.github.com>
2024-09-08 12:36:26 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -5,15 +5,17 @@
 
 # Install OpenJDK 11
 echo "Installing OpenJDK 11..."
+which java
+
 USER root
 # RUN commands
-USER 1001
+#USER 1001
 #apt-get update
 #apt-get install -y openjdk-11-jdk
 
 # Set JAVA_HOME environment variable
-export JAVA_HOME=/usr/local/openjdk-8
-export PATH=$JAVA_HOME/bin:$PATH
+#export JAVA_HOME=/usr/local/openjdk-8
+#export PATH=$JAVA_HOME/bin:$PATH
 # export LD_LIBRARY_PATH=$JAVA_HOME/lib:$LD_LIBRARY_PATH
 # export LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk-amd64/lib/server
 # Verify Java installation
